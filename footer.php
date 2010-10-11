953e44bd9bee4c9d979b6d64dce88b2eb9c5f783
Adriaan Pelzer <adriaan@wewillraakyou.com>
2010-10-11 18:58:12 +0100

    Fixed footer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
         <footer>
             <span class="footer_link"><a href="http://www.youtube.com/user/wewillRAAKyou">RAAK on Youtube</a></span>
             <span class="footer_seperator">|</span>
-            <span class="footer_link"><a href="http://www.twitter.com/RAAKonteurs">RAAK on Twitter</a></span>
+            <span class="footer_link"><a href="http://www.twitter.com/RAAKonteurs">Twitter</a></span>
             <span class="footer_seperator">|</span>
-            <span class="footer_link"><a href="http://www.linkedin.com/companies/raak">RAAK on LinkedIn</a></span>
+            <span class="footer_link"><a href="http://www.linkedin.com/companies/raak">LinkedIn</a></span>
             <span class="footer_seperator">|</span>
-            <span class="footer_link"><a href="http://www.facebook.com/pages/RAAK/167105176615">RAAK on Facebook</a></span>
+            <span class="footer_link"><a href="http://www.facebook.com/pages/RAAK/167105176615">Facebook</a></span>
             <span id="footer_copyright">&copy; RAAK 2010</span>
         </footer>
     </body>
