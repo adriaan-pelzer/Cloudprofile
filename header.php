96137e8f9ace5b87c8ed3d77a8e1bf0c87aaaff4
Adriaan Pelzer <adriaan@wewillraakyou.com>
2010-10-12 15:46:14 +0100

    Fix typo in header

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <body>
         <header>
             <h1><a href="/">OMNII</a></h1>
-            <h4 style="padding: 4px; padding-top:0px; padding-bottom: 8px; font-size:11px;text-align:center;">Show us your's & we will show you everybody's</h4>
+            <h4 style="padding: 4px; padding-top:0px; padding-bottom: 8px; font-size:11px;text-align:center;">Show us yours & we will show you everybody's</h4>
         </header>
         
         <div style="color: #CCC; text-align:center; margin-top: 5px; margin-bottom: 10px;">built by <a href="http://wewillraakyou.com">RAAK</a></div>
