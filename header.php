3c029c6d1db12101aadf719be243473be9ce5dba
Adriaan Pelzer <adriaan@wewillraakyou.com>
2010-10-12 14:19:59 +0100

    Strapline

--- header.php
+++ header.php
@@ -6,8 +6,10 @@
     </head>
     <body>
         <header>
-            <h1>OMNII</h1>
+            <h1><a href="/">OMNII</a></h1>
+            <h4 style="padding: 4px; padding-top:0px; padding-bottom: 8px; font-size:11px;text-align:center;">Show us yours & we will show you everybodies</h4>
         </header>
+        
         <div style="color: #CCC; text-align:center; margin-top: 5px; margin-bottom: 10px;">built by <a href="http://wewillraakyou.com">RAAK</a></div>
         <div style="color: #CCC; font-size: 12px; text-align:center;">an API enabled application</div>
 
